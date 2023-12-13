457f11fd3b1b6cd2310b250a2582d4ef061ee5b7
dasdo <adrian12reye@gmail.com>
2023-12-14 00:18:55 +0100

    uso de telnet para recibir mensaje

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).expect("Failed to read from socket");
+    let received_message = String::from_utf8_lossy(&buffer[..]);
+    println!("Received message: {}", received_message);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind to address");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                std::thread::spawn(|| {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => {
+                eprintln!("Error accepting connection: {}", e);
+            }
+        }
+    }
 }
